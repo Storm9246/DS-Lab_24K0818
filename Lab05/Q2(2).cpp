a6e58dd1bd393aa9935131ff1daeb8043ab06d9f
Storm9246 <k240818@nu.edu.pk>
2025-09-19 12:10:17 +0500

    Q2(2).cpp

--- Lab05/Q2(2).cpp
+++ Lab05/Q2(2).cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int funcB(int x); // forward declaration
+int funcB(int x);
 
 int funcA(int x) {
     if (x <= 0) {
         return 1;
     }
-    // changed structure a little (variables + clearer flow)
+    
     int part1 = x - funcA(x - 1);
     int part2 = funcB(x - 1);
     return part1 + part2;
@@ -17,7 +17,7 @@ int funcB(int x) {
     if (x <= 0) {
         return 1;
     }
-    // reorganized calculation (use variables)
+    
     int part1 = x + funcA(x - 1);
     int part2 = funcB(x - 1);
     return part1 + part2;
