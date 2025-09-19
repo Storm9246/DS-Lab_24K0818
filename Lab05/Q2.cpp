fce987ba69fb9b777a2e4d1bf231be29cc0060d6
Storm9246 <k240818@nu.edu.pk>
2025-09-19 12:02:50 +0500

    Create Q2.cpp

---
+++ Lab05/Q2.cpp
@@ -0,0 +1,48 @@
+// Task 2:
+// 1. Write a simple C++ program that demonstrates direct recursion. Create a function
+// printNumbers(int n) that prints numbers from n down to 1 by calling itself
+// directly.
+// 2. Next, demonstrate indirect recursion by creating two functions, functionA(int n)
+// and functionB(int n). functionA should call functionB, and functionB
+// should call functionA, forming a cycle that prints numbers in a specific pattern.
+// Ensure both programs have a base condition to prevent infinite loops.
+
+// Task 2:
+// 1. Write a simple C++ program that demonstrates direct recursion. Create a function
+// printNumbers(int n) that prints numbers from n down to 1 by calling itself
+// directly.
+// 2. Next, demonstrate indirect recursion by creating two functions, functionA(int n)
+// and functionB(int n). functionA should call functionB, and functionB
+// should call functionA, forming a cycle that prints numbers in a specific pattern.
+// Ensure both programs have a base condition to prevent infinite loops
+
+
+
+
+#include <iostream>
+using namespace std;
+
+void Pnum(int n);
+
+int main() 
+{
+	int n;
+	cout<<"enter number: ";
+	cin>>n;
+	Pnum(n);
+}
+
+void Pnum(int n) 
+{
+	if (n==1) 
+	{
+		cout<<1<<endl;
+		return ;
+	}
+	else 
+	{
+		cout<<n<<endl;
+		Pnum(n-1);
+	}
+}
+
