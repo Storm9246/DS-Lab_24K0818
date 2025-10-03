394bed98dca3150d87779751e8b63cf1b616160e
Storm9246 <k240818@nu.edu.pk>
2025-10-03 22:20:55 +0500

    Create Q3.cpp

---
+++ Lab05/Q3.cpp
@@ -0,0 +1,44 @@
+/*1. Implement a C++ function sumTail(int n, int total) that calculates the sum
+of numbers from 1 to n using tail recursion. The recursive call should be the last
+operation in the function.
+*/
+
+#include <iostream>
+using namespace std;
+
+int sumTail(int n, int total)
+{
+    if(n <= 0) return total;
+    return sumTail(n - 1, total + n);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << sumTail(n, 0) << endl;
+    return 0;
+}
+
+/*2. Implement another function sumNonTail(int n) that calculates the same sum
+using non-tail recursion. This function should perform an operation (e.g., addition) after
+the recursive call returns. Compare the two implementations and explain the difference
+in their call stacks
+*/
+
+#include <iostream>
+using namespace std;
+
+int sumNonTail(int n)
+{
+    if(n <= 0) return 0;
+    return n + sumNonTail(n - 1);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << sumNonTail(n) << endl;
+    return 0;
+}
